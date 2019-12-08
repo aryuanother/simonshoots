4483aa62c6bc27a802e016815ed36f00c5c33b64
aryuanother <aryuanother@gmail.com>
2019-12-08 22:31:11 +0900

    touch event test

--- src/framework/input.ts
+++ src/framework/input.ts
@@ -7,6 +7,7 @@ let px:number, py:number
 let dragging:boolean, pd:boolean
 
 function ts(e:TouchEvent){
+    alert("touched")
     e.preventDefault()
     px = mousex = e.touches[0].clientX
     py = mousey = e.touches[0].clientY
